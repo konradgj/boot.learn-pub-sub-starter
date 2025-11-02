59020bba05b46bc4c2fdc81eeee529b8630b381d
konradgj <konrad.gjerstad@gmail.com>
2025-11-02 15:05:29 +0100

    CH2: Message Brokers L7: MQTT and STOMP

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const connectionString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("could not dial connection: %v", err)
+	}
+	defer connection.Close()
+	fmt.Println("Successfully created a connection.")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("\nClosing connection...")
+	fmt.Println("Shutting down...")
 }
